132330b0c98b7180c35c84851bbd636c5e22788c
juvelist <juvelist@list.ru>
2023-11-22 12:41:40 +0400

    add react, some changes

--- js/main.js
+++ js/main.js
@@ -4,26 +4,6 @@ $('.print__btn').click(function (event) {
     event.preventDefault();
 });
 
-// skills percent
-$('.right-block__skills-percent').each(function()
-{
-    var val = $(this).data('label');
-    $(this).css('width', val);
-
-});
-
-// skills mouseover
-$('.right-block__skills-text').mouseover(function()
-{
-    var val = $(this).next('.right-block__skills-indicator').find('.right-block__skills-percent').data('label');
-    $(this).next('.right-block__skills-indicator').find('.right-block__skills-percent').css('width', '1');
-
-    $('.right-block__skills-text').mouseout(function()
-    {
-        $(this).next('.right-block__skills-indicator').find('.right-block__skills-percent').css('width', val);
-    });
-});
-
 // up button
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 200) {
